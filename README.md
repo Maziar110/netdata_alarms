2a942ce5717d7620e7c8167d28af12428b5ba5fe
Maziar Shahsavanpour <maziar.sh110@riseup.net>
2019-12-15 21:02:49 +0330

    Add api.py to repository \n modify readme.md \n add sample ip host file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# netdata_alarms
+# What this project does?
+
+If you work in a monitoring role or any other parts of a technical team that works with NetData as a monitoring tools on your server(s), you might need sometimes just to see your NetData alarms.
+## Requirement:
+
+1- you need python3.x to be installed:
+``` bash
+apt update
+apt install python3.6
+```
+2- You need pip3 to be installed:
+``` bash
+wget -P . https://bootstrap.pypa.io/get-pip.py
+python3 ./get-pip.py
+```
+3- you need request package to be installed: 
+```
+pip3 install requests
+```
+## How to run the project:
+You need to have a text file with the name of ` ip_hosts.txt ` beside the project(in the same directory) and put your servers IPs in that as it's shown in `ip_hosts_sample.txt`'s file, the project will add :19999(default port of netdata- which you can manualy change in the code by yourself) at the end of the file and so on.
+### Steps:
+1- Clone the project:
+``` bash
+git clone https://github.com/Maziar110/netdata_alarms.git
+```
+2- Head to the project directory
+``` bash
+cd ./netdata_alarms
+```
+3- If you have all the requirement successfully installed, run the bellow command in the project's directory:
+``` bash
+python3 api.py
+```
+it prints out all critical alarms of the mentioned servers in the `ip_hosts.txt` file.
+
+__
+
+Let me know if there's any issue or furthur talk.
+you can be in touch with me through:
+
+[Linkedin](https://www.linkedin.com/in/maziar-shahsavanpour-a4210088/)
+
+[Whatsapp](https://api.whatsapp.com/send?phone=+989156262067)
---
+++ api.py
@@ -0,0 +1,74 @@
+import requests
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+file = open("./ip_hosts.txt", "r")
+ips = file.readlines()
+
+
+def host_extractor(ip):
+    # We'll develop this method later in order to get all hosts addresses and use them in the project
+    ip2 = str(ip).replace("\n", "")
+    host_ip = str(ip2) + ":19999"
+    return host_ip
+
+
+alarm_req = ""
+info_req = ""
+for ip in ips:
+
+    # To set host automatically, uncomment bellow line:
+    host = host_extractor(ip)
+
+    # Set host manually, TO use it uncomment bellow line:
+    # host = '172.16.77.20:19999'
+
+    alarms_url = "http://" + host + "/api/v1/alarms"
+    info_url = "http://" + host + "/api/v1/info"
+
+    try:
+        alarm_req = requests.get(url=alarms_url)
+        info_req = requests.get(url=info_url)
+        data = alarm_req.json()
+        alarms_details = data["alarms"]
+        alarms = alarms_details.get("info")
+
+        info = info_req.json()
+        server_name = info["mirrored_hosts"]
+        for items in alarms_details:
+            if alarms_details[items]["status"] == "CRITICAL":
+                print(color.BOLD, color.GREEN, "\n", "We have bellow errors for host " + host +
+                      " in the section of: " + items, ":", color.END)
+                alarms = alarms_details[items]["info"]
+                print(color.BOLD, color.RED, alarms, color.END, "\n")
+
+    except Exception as e:
+        if alarm_req != "":
+            data = alarm_req.json()
+            alarms_details = data["alarms"]
+            alarms = alarms_details.get("info")
+
+            for items in alarms_details:
+                if alarms_details[items]["status"] == "CRITICAL":
+                    print(color.BOLD,color.GREEN,"\n", "We have bellow errors for host " + host +
+                          " in the section of: " + items, ":", color.END)
+                    alarms = alarms_details[items]["info"]
+                    print(color.BOLD, color.RED, alarms, color.END, "\n")
+
+        print(color.PURPLE ,color.BOLD,"Exception happend: ",color.END, color.CYAN, str(e)
+              , " **Alaram method result:" , str(alarm_req)
+                 , " and info method result:" , str(info_req), color.END)
+
+file.close()
